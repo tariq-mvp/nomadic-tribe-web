e1da52e27024e6ed12fdbbc6d01e2f1053fd003a
tariq-mvp <tariq@mvp-apps.ae>
2024-02-29 11:41:35 +0300

    updates

--- src/pages/home2.js
+++ src/pages/home2.js
@@ -4,32 +4,32 @@
 // import BrandTwo from "@/components/BrandTwo/BrandTwo";
 // import CounterOne from "@/components/CounterOne/CounterOne";
 // import DestinationsTwo from "@/components/DestinationsTwo/DestinationsTwo";
-// import Layout from "@/components/Layout/Layout";
+import Layout from "@/components/Layout/Layout";
 // import NewsTwo from "@/components/NewsTwo/NewsTwo";
 // import PopularToursTwo from "@/components/PopularTours/PopularToursTwo";
 // import SingaporeTour from "@/components/SingaporeTour/SingaporeTour";
 // import ThreeIconBox from "@/components/ThreeIconBox/ThreeIconBox";
 // import TourTypes from "@/components/TourTypes/TourTypes";
 // import VideoTwo from "@/components/VideoTwo/VideoTwo";
-// import React from "react";
+import React from "react";
 
-// const Home2 = () => {
-//   return (
-//     <Layout pageTitle="Home Two">
-//       <BannerTwo />
-//       <ThreeIconBox />
-//       <PopularToursTwo />
-//       <SingaporeTour />
-//       <VideoTwo />
-//       <CounterOne />
-//       <DestinationsTwo />
-//       <AboutTwo />
-//       <TourTypes />
-//       <BrandTwo />
-//       <NewsTwo />
-//       <BookNow />
-//     </Layout>
-//   );
-// };
+const Home2 = () => {
+  return (
+    <Layout pageTitle="Home Two">
+      {/* <BannerTwo />
+      <ThreeIconBox />
+      <PopularToursTwo />
+      <SingaporeTour />
+      <VideoTwo />
+      <CounterOne />
+      <DestinationsTwo />
+      <AboutTwo />
+      <TourTypes />
+      <BrandTwo />
+      <NewsTwo />
+      <BookNow /> */}
+    </Layout>
+  );
+};
 
-// export default Home2;
+export default Home2;
